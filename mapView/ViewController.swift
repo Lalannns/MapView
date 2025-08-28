0046803933400a987a3680ae4ac0fdb10f8e32c6
Allan Auezkhan <allanauezkhan@MacBook-Air-Allan.local>
2025-08-29 02:30:07 +0500

    first commit

---
+++ mapView/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  mapView
+//
+//  Created by Allan Auezkhan on 29.08.2025.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
---
+++ mapViewTests/mapViewTests.swift
@@ -0,0 +1,17 @@
+//
+//  mapViewTests.swift
+//  mapViewTests
+//
+//  Created by Allan Auezkhan on 29.08.2025.
+//
+
+import Testing
+@testable import mapView
+
+struct mapViewTests {
+
+    @Test func example() async throws {
+        // Write your test here and use APIs like `#expect(...)` to check expected conditions.
+    }
+
+}
